f387a24e4414cad31908e107685618985a63b48e
NawaShoLin <twelendil@gmail.com>
2015-05-05 15:58:23 +0800

    create nn-driver

---
+++ src/scripts/model/nn-driver.js
@@ -0,0 +1,41 @@
+var NNDriver = function(xfuns, ms, sigmas, weights) {
+    if (!(ms.length == sigmas.length &&
+        sigmas.length == weights.length)) {
+        throw "Arguments do not match.";
+    }
+
+    var phiOfI = function(i, x) {
+        return phi(x, ms[i], sigmas[i]);
+    };
+
+    var phi = function(x, m, sigma) {
+        if (x.length != m.length) {
+            throw "vector 'x' do not match vector 'm'";
+        }
+
+        // s = (|x - m| ^ 2)
+        var s = 0;
+        for (var i = 0; i < x.length; x += 1) {
+            var diff = x[i] - m[i];
+            s += diff * diff;
+        }
+
+        return Math.exp(-(s / (2 * sigma * sigma)));
+    };
+
+    // F()
+    return function() {
+        // x is a vector : (x1, x2, ... ,xp)
+        // args to f is sensor inputs
+        var x = [];
+        xfuns.forEach(function(xfun) {
+            x.push(xfun.apply(null, arguments));
+        });
+
+        var sum = 0;
+        weights.forEach(function(weight, i) {
+            sum += phiOfI(i, x) * weight;
+        });
+        return sum;
+    };
+};
